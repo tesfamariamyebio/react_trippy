bae24a40fe67c938d8b19bff3713de0e3f3188b2
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-08-14 16:05:25 +0200

    'trippy'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import Home from '../components/core/Home';
 import HomeContainer from './containers/HomeContainer';
 
 import{
@@ -10,56 +9,17 @@ import{
 }
 from 'react-router-dom';
 class App extends React.Component{
-  // constructor(props){
-  //   super(props);
-  //   this.state={
-  //     cities:[],
-    
-  //   }
-  // }
-  // componentDidMount(){
-  //   const url='http://localhost:3001/api/home';
-  //   console.log(url);
-  //   fetch(url)
-  //   .then(res=>res.json())
-  //   .then(json=>{
-  //     console.log(json);
-  //     this.setState({
-  //       cities:json.cities,
-       
-  //     })
-  //   });
-  // }
   render(){
     return (
-     
-        <Router>
-        <div>
-          <Switch>
-            <Route path="/">
-            <HomeContainer />
-            </Route>
-            {/* <Route path="/">
-            <Home />
-            </Route> */}
-            
-          </Switch>
-        </div>
+       <Router>
+          <div>
+            <Switch>
+              <Route path="/">
+              <HomeContainer />
+              </Route>
+            </Switch>
+          </div>
         </Router>
-       
-
-    //   <div>{this.state.cities.map((city)=>{
-    //     console.log(city);
-    //     return (
-    //           <div> 
-    //           <img src={'http://localhost:3001/' + city.source}/>
-             
-    //             <p> {city.name}</p>
-    //             <p> {city.slug}</p>
-    //           </div>
-    //     )
-    //   })}
-    // </div>
     );
   
 }
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -9,26 +9,14 @@ import Card from '../components/city/Card';
      render(){
          return(
         <div
-        className='container-fluid'
-        style={{
-          fontFamily: 'Montserrat'
-        }}>
-        {/* <div
-          className='row'>
-          <div
-            className={'col-lg-4 col-md-8 col-12'}>
-            <p>
-              Je vérifie que les 2 mots : Voleur et <span style={{fontFamily: 'serif' }}>Voleur</span> ont une police d'écriture différente
-            </p>
-          </div>
-          <div
-            className='col-lg-8 col-md-4 col-12'>
-            Une autre phrase
-          </div>
-        </div> */}
+            className='container-fluid'
+            style={{
+              fontFamily: 'Montserrat'
+            }}>
+          
               The cities that you might like them
               <Card /> 
-   </div>
+        </div>
      
          );
      }
